aea2ba08c23763ca0da315b50d93dbefd277444f
ncar06 <144852581+ncar06@users.noreply.github.com>
2023-09-26 17:28:37 -0600

    added servo movement code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpModeKP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpModeKP.java
@@ -2,36 +2,45 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 
 public class TestOpModeKP extends LinearOpMode {
+    //imu = hardwareMap.get(Gyroscope.class, "imu");
+//motorTest = hardwareMap.get(DcMotor.class, "motorTest");
+//digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
+//sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
     //private Gyroscope imu;
     //private DcMotor motorTest;
     //private DigitalChannel digitalTouch;
     //private DistanceSensor sensorColorRange;
-    private Servo servoTest;
+    private Servo servoTest = hardwareMap.get(Servo.class, "servo");
+    private long CYCLE_MS;
+    private double position;
 
 
     @Override
     public void runOpMode() {
-        //imu = hardwareMap.get(Gyroscope.class, "imu");
-        //motorTest = hardwareMap.get(DcMotor.class, "motorTest");
-        //digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
-        //sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
-        servoTest = hardwareMap.get(Servo.class, "servo");
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
-
+    CYCLE_MS=100;
+    position=0.0;
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-            telemetry.addData("Status", "Running");
+            telemetry.addData("Status", "Not running");
             telemetry.update();
-
+            servoTest.setPosition(position);
+            position+=0.1;
+            if(position>1.0){
+                position=0.0;
+            }
+            sleep(CYCLE_MS);
+            idle();
         }
     }
 }
