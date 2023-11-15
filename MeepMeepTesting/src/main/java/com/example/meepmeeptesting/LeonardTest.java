66238e5273426d2489ec5c2fc4b3f8a98ca36ab8
KaiaPetersen <26kpeterson@pistem.org>
2023-11-14 17:04:37 -0700

    added classes RobotAutoDriveToAprilTagOmni and TestOpModeKP

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/LeonardTest.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/LeonardTest.java
@@ -14,19 +14,19 @@ public class LeonardTest {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
+                        drive.trajectorySequenceBuilder(new Pose2d(12, -62, 0))
                                 .turn(Math.toRadians(90))
                                 .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
+                                //.forward(30)
+                                //.turn(Math.toRadians(90))
+                                //.forward(30)
+                                //.turn(Math.toRadians(90))
+                                //.forward(30)
+                                //.turn(Math.toRadians(90))
                                 .build()
                 );
 
-        meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
+        meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
